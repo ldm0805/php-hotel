51f88839fc82979bbe525994fb13b00acfac3ab0
ldm0805 <leonardodimaggio94@gmail.com>
2023-01-30 14:24:31 +0100

    Esercizio base completo

--- index.php
+++ index.php
@@ -40,10 +40,44 @@
 
     ];
 
-    foreach($hotels as $hotel){
-    foreach($hotel as $item){
-        echo $item."<br>";
-    }
-}
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>Array php</title>
+</head>
+    <body>
+        <table class="mt-3">
+            <thead>
+                <th>Nome</th>
+                <th>Descrizione</th>
+                <th>Voto</th>
+                <th>Distanza dal centro</th>
+                <th>Parcheggio</th>
+            </thead>
+            <tbody>
+                <?php foreach($hotels as $hotel){?>
+                    <tr>
+                        <td><?php echo $hotel['name'];?></td>
+                        <td><?php echo $hotel['description'];?></td>
+                        <td><?php echo $hotel['vote'];?></td>
+                        <td><?php echo $hotel['distance_to_center'];?></td>
+                        <td>
+                            <?php if($hotel['parking']){
+                                echo 'Si';
+                            }else{
+                                echo 'No';
+                                }; ?>
+                        </td>
+                    </tr>
+                <?php }?>
+
+            </tbody>
+        </table>
+    </body>
+</html>
